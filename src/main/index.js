2faf70139764aa844ee28d37f2c8db0eea6da9ea
Nail0606 <c9010912@gmail.com>
2023-03-06 23:16:55 +0900

    계획 변동

--- src/main/index.js
+++ src/main/index.js
@@ -1,7 +1,18 @@
+import styled from "styled-components";
+
+const StyledBanner = styled.div`
+  background-color: gray;
+  width: 1000px;
+  height: 400px;
+`;
+
+const PageTitle = styled.h1``;
+
 function MainPage() {
   return (
     <div>
-      <p>가나다</p>
+      <h1>일기장</h1>
+      <StyledBanner />
     </div>
   );
 }
