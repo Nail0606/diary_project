74cc6b8f025a137f01c3a39fc0984e1438464bd5
Nail0606 <c9010912@gmail.com>
2023-03-08 23:54:12 +0900

    feat : flex 레이아웃 적용

--- src/main/index.js
+++ src/main/index.js
@@ -3,17 +3,40 @@ import styled from "styled-components";
 const StyledBanner = styled.div`
   background-color: gray;
   width: 1000px;
-  height: 400px;
+  height: 100%;
+  margin: 10px;
 `;
 
-const PageTitle = styled.h1``;
+const StyledDiv = styled.div`
+  background-color: lightblue;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const MainLine = styled.hr`
+  width: 1000px;
+  color: gray;
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 1000px;
+`;
 
 function MainPage() {
   return (
-    <div>
-      <h1>일기장</h1>
+    <StyledDiv>
+      <Header>
+        <h1>My_Album</h1>
+        <h1>My_Album2</h1>
+      </Header>
+      <MainLine />
       <StyledBanner />
-    </div>
+    </StyledDiv>
   );
 }
 
